e5132d60fecc0e904837c95bc8188d4d309f7c3b
rodriguezr18 <rodriguez.rudy219@gmail.com>
2021-02-16 17:06:50 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import List from './components/List'
 
-function App() {
+class App extends React.Component {
+  // tracking user input: using a form or an input field
+  // onChange is probably needed
+  // onSubmit is definitely needed
+  // ill probably need a preventDefault
+
+
+  // state just to keep my information the same and only change when I need it to change
+
+  constructor(props){
+    super(props)
+
+    this.state = {
+      noteText: '',
+      notes:[]
+    }
+  }
+
+  onChange = (event) => {
+    this.setState({
+      noteText:event.target.value
+    })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    this.setState({
+      noteText:'',
+      notes:[...this.state.notes,this.state.noteText]
+    })
+    console.log(this.state.notes)
+  }
+
+  render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+<div className="header">
+      <div className ="wrapper">
+      <img src = "https://roadtohire.org/static/80b65f57d3d3886872d58a21bf7ac4be/r2h-icon.png" alt="R2H Logo" />
+      <p className="title">Road To Hire</p>
+      </div>
     </div>
+    <div className="app">
+      <div className="app-wrapper">
+        <p className="app-title">To-Do List</p>
+        <form className ="App" onSubmit = {this.onSubmit}>
+          <input value = {this.state.noteText} onChange = {this.onChange} placeholder="Type here to add a task" type="text" id="box" />
+          <br />
+        </form>
+        <br />
+        <List notes = {this.state.notes} />
+</div>
+</div>
+</div>
   );
 }
-
+}
 export default App;
---
+++ src/components/List.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const List = (props) =>{
+    return(
+        <ul>
+            {
+            props.notes.map((note, index) => 
+            <li key = {index}>
+            {note}
+            </li> )
+            }
+
+        </ul>
+    )
+
+}
+
+export default List
